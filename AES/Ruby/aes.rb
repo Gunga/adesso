ee97863cd24d3568bee17944d955ac08397a5479
Joan Torres <joan.torres@upr.edu>
2014-11-15 08:28:20 -0400

    Format terminal output

--- AES/Ruby/aes.rb
+++ AES/Ruby/aes.rb
@@ -71,7 +71,7 @@ class AESCipher
       end
     end
 
-    output
+    output.join(" ")
   end
 
   private
@@ -149,4 +149,7 @@ class AESCipher
 end
 
 crypt = AESCipher.new(cipher_key: "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c", nk: 4, nr: 10, nb: 4)
-crypt.cipher("32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34")
+puts "input:  32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34"
+puts "key:    2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c"
+print "output: "
+puts crypt.cipher("32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34")
